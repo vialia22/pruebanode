0200f1a2f944b8ed3be232db3087c0c8a84c8985
Adri <abs@dsad.com>
2023-05-21 22:01:35 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require("http")  
+const server = http.createServer(requestController)
+
+
+
+function requestController(){
+    console.log("recibimos una nueva request!!");
+} 
+server.listen(4000,() => {console.log("server on port 4000")}); 

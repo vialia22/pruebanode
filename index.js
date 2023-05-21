e9d4075d77f267f2e5116ec9d91091cbb9a4e290
Adri <abs@dsad.com>
2023-05-21 23:36:02 +0200

    cambio a yarn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const server = http.createServer(requestController)
 
 
 function requestController(){
-    console.log("recibimos una nueva request!!!");
+    console.log("recibimos una nueva request!!");
 } 
 server.listen(4000,() => {console.log("server on port 4000")}); 

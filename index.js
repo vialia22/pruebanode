fac14139ee6c0e1ff333ded616daaea759491dc1
Adri <abs@dsad.com>
2023-05-22 13:38:29 +0200

    comentarios

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require ('dotenv').config();
 const http = require("http")  
 //const server = http.createServer(requestController)
 
-const server = http.createServer((req,res)=>{
+const server = http.createServer((req,res)=>{   //cambiamos esto para que el servidor mande una respuesta y un cosole log
     console.log('solicitud nueva');
     res.end("Hola mundo");
 })
@@ -11,11 +11,11 @@ const server = http.createServer((req,res)=>{
 
 
 
-function requestController(){
+function requestController(){    //si ponemos lo de arriba estaria en desuso
     console.log("recibimos una nueva request!!");
 
 }
 
-const PORT = process.env.PORT; 
+const PORT = process.env.PORT;  //llamamos a la variable port del modulo process
 
-server.listen(PORT,() => {console.log("server on port " + PORT)}); 
+server.listen(PORT,() => {console.log("server on port " + PORT)});   //escuchamos por el puerto definido en .env o el que corresponda en produccion
